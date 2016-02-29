3e33c7a6e78b1e7400d98c3c17b9145d9ff22873
Dim Skylos <doiranl@gmail.com>
2016-02-29 12:31:26 +0200

    added addfunction to code

--- gated_commit/src/main/java/nokia/gated_commit/App.java
+++ gated_commit/src/main/java/nokia/gated_commit/App.java
@@ -12,6 +12,7 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	App a = new App();
+        System.out.println(a.addNumbers(6, 4));
     }
 }
