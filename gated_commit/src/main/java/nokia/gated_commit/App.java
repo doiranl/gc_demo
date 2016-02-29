e465f49a4ff265565aac64ea8d97e29498de7dc6
Dim Skylos <doiranl@gmail.com>
2016-02-29 11:57:48 +0200

    added maven project

---
+++ gated_commit/src/main/java/nokia/gated_commit/App.java
@@ -0,0 +1,17 @@
+package nokia.gated_commit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int addNumbers(int a, int b) {
+		return a + b;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
